0745389a9cac9693ba371076d0a101aa6b1a4aef
Jan Matter <jan.matter@outlook.com>
2025-03-20 07:34:31 +0100

    test ci/cd

--- deployment/pipelines/build.sh
+++ deployment/pipelines/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sudo snap install aws-cli --classic
-
 docker build -t "$TES_IMAGE_TAG" . 
 
 case $CURRENT_BRANCH in
@@ -14,6 +12,9 @@ case $CURRENT_BRANCH in
   *) echo "This image won't be pushed" >&2
     exit 0;
 esac
+
+export TES_IMAGE="046776365295.dkr.ecr.eu-central-1.amazonaws.com/starthack-$env/api"
+export TES_IMAGE_TAG="$TES_IMAGE:$(echo "$RUN_ID" | tr -d '{}')"
 
 docker tag "$TES_IMAGE_TAG" "$TES_IMAGE:$env"
 docker push "$TES_IMAGE_TAG"
