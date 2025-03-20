17e4a71e9cbee9499a20d12fe78c75bd251173f2
Jan Matter <jan.matter@outlook.com>
2025-03-20 07:53:16 +0100

    cicd

--- deployment/pipelines/k8s_deploy.sh
+++ deployment/pipelines/k8s_deploy.sh
@@ -10,8 +10,6 @@ case $GITHUB_REF_NAME in
     exit 0;
 esac
 
-export TES_IMAGE="046776365295.dkr.ecr.eu-central-1.amazonaws.com/starthack-$env/api:$GITHUB_SHA"
-
 git config --global user.email "jan.matter@outlook.com"
 git config --global user.name "Jan Matter"
 
@@ -20,7 +18,7 @@ cd starthack-2025-deployment
 git checkout $GITHUB_REF_NAME
 
 cd helmcharts/starthack-backend-chart
-sed -i "s/^.*image:.*$/image: $TES_IMAGE/" values.yaml
+sed -i "s/^.*image:.*$/image: "046776365295.dkr.ecr.eu-central-1.amazonaws.com/starthack-$env/api:$GITHUB_SHA/" values.yaml
 git add values.yaml
 git commit -m "Update image tag for deployment $GITHUB_SHA"
 git push origin $GITHUB_REF_NAME
